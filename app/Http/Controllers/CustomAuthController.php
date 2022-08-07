46890b9ab48efc21ae38e6efbfd161e14f58b74e
Maegan <maegan.parales@usjr.edu.ph>
2022-08-08 02:01:41 +0800

    created CustomAuthController.php file
    for login and register

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomAuthController extends Controller
+{
+    public function home()
+    {
+        return view('homepage');
+    }
+
+    public function index()
+    {
+        return view('login');
+    }
+}
