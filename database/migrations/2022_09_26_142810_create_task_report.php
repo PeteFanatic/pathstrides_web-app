1839c19027f25c5802cd4635869fdba16246db6b
Zeki <zeki.oralde@usjr.edu.ph>
2022-09-27 22:53:13 +0800

    Updated Database with task_Report

---
+++ database/migrations/2022_09_26_142810_create_task_report.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_report', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->string('report_text');
+            $table->binary('report_image');
+            $table->timestamps();
+        });
+
+        $table->foreign('user_id')->references('user_id')->on('users');
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_report');
+    }
+};
