a35f056256c612a5e9cdc9598ccd9ca7f69ff9bf
Zeki <zeki.oralde@usjr.edu.ph>
2022-10-31 19:27:46 +0800

    Geolocation(with marker) cannot plot yet

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -21,8 +21,10 @@
         <input type="text" name="points" id="points" class="form-control"></br>
         <label>Location:</label></br>
          <div class="location">
-        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#myModal">
-         Select Location
+       
+        <!-- Button to Open the Modal -->
+        <button type="button" id="open" class="btn btn-success" data-toggle="modal" data-target="#myModal" data-lat="5.134515" data-long="97.151759">
+          Select Location
         </button>
         </br>
         <label for="user_id">Emp id:</label></br>
@@ -59,26 +61,113 @@
 <div class="modal" id="myModal">
     <div class="modal-dialog">
       <div class="modal-content">
-      
+
         <!-- Modal Header -->
         <div class="modal-header">
-          <h4 class="modal-title">Choose Location </h4>
+          <h4 class="modal-title">Select Location</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
-        
+
         <!-- Modal body -->
         <div class="modal-body">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d116862.54554679655!2d90.40409584970706!3d23.749000170125925!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1550040341458" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+           <!-- show map here -->
+            <div id="map"></div>
         </div>
-        
+
         <!-- Modal footer -->
         <div class="modal-footer">
           <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
         </div>
-        
+
       </div>
     </div>
   </div>
-  
+
 </div>
+
+<script>
+ // This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+let map, infoWindow;
+//{ lat: -34.397, lng: 150.644 }
+function initMap(myLoc) {
+  var marker = new google.maps.Marker({
+            position: myLoc
+        });
+      var opt = {
+            center: myLoc,
+            zoom: 6,
+        };
+  map = new google.maps.Map(document.getElementById("map"),opt), {
+    position:myLoc,
+    zoom: 6,
+    zoomControl: true,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      mapTypeIds: ["roadmap", "terrain"],
+    },
+    scaleControl: true,
+    streetViewControl: true,
+    streetViewControlOptions: {
+      position: google.maps.ControlPosition.LEFT_TOP,
+    },
+    rotateControl: true,
+    fullscreenControl: true,
+   
+  };
+  marker.setMap(map);
+  
+  infoWindow = new google.maps.InfoWindow();
+
+  
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+
+$("#open").click(function(){
+// get latitude and longitude that pass from open modal button
+    initMap(new google.maps.LatLng($(this).data('lat'), $(this).data('long')));
+    });
+
+</script>
 @stop
