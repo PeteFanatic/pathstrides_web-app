ef411f9f3e3783a42d840e13d03e802d75c94957
Maegan <maegan.parales@usjr.edu.ph>
2022-12-08 16:02:02 +0800

    department ui

--- resources/views/departments/edit.blade.php
+++ resources/views/departments/edit.blade.php
@@ -8,9 +8,9 @@
         {!! csrf_field() !!}
         @method("PATCH")
         <label>department ID</label></br>
-        <input type="text" name="ann_id" id="ann_id" value="{{$departments->ann_id}}" id="ann_id" /></br>
-        <label>department Title:</label></br>
-        <input type="text" name="ann_title" id="ann_title" value="{{$departments->ann_title}}" class="form-control"></br>
+        <input type="text" name="dep_id" id="dep_id" value="{{$departments->ann_id}}" id="ann_id" /></br>
+        <label>department Name:</label></br>
+        <input type="text" name="dep_name" id="dep_title" value="{{$departments->ann_title}}" class="form-control"></br>
         <label>department Description:</label></br>
         <input type="text" name="ann_desc" id="ann_desc" value="{{$departments->ann_desc}}" class="form-control"></br>
         
